842f47c059b18e6f9ef9bf539396e552ee605098
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-08 11:53:50 -0700

    initial commit

---
+++ moreDotLessDash.js
@@ -0,0 +1,9 @@
+// Your code here
+
+
+
+console.log(moreDotLessDash('2-D arrays are fun. I think.'));           // true
+console.log(moreDotLessDash('Morse code is great.'));                   // true
+console.log(moreDotLessDash('.... . -.--'));                            // true
+console.log(moreDotLessDash('.--. .-. --- --. .-. .- -- -- . .-.'));    // false
+console.log(moreDotLessDash('high-flying acrobat.'));                   // false
