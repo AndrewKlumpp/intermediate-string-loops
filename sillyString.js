7cb6f9ed5256a2d855f20eea31a9e1c060e77d4c
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-08 11:38:09 -0700

    initial commit

---
+++ sillyString.js
@@ -0,0 +1,9 @@
+// Your code here
+
+
+console.log(sillyString('stop'));       // stobop
+console.log(sillyString('that'));       // thabat
+console.log(sillyString('can'));        // caban
+console.log(sillyString('cats'));       // cabats
+console.log(sillyString('italy'));      // ibitabaly
+console.log(sillyString('scooter'));    // scobooboteber
