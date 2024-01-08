e5cc79cd181945046435acc72208f3ca0ac1b4b0
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-08 15:51:08 -0700

    initial commit

---
+++ hasThreeVowels.js
@@ -0,0 +1,8 @@
+// Your code here
+
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
