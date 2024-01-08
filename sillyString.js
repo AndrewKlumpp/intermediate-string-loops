a0d369f845eca99dfff27853865c1f9b19d58a3f
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-08 11:51:43 -0700

    finished silly string

--- sillyString.js
+++ sillyString.js
@@ -1,4 +1,16 @@
-// Your code here
+function sillyString(str) {
+  let newStr = "";
+  let vowels = "aeiou";
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (vowels.includes(char)) {
+      newStr = newStr + char + "b" + char;
+    } else {
+      newStr = newStr + char;
+    }
+  }
+  return newStr;
+}
 
 
 console.log(sillyString('stop'));       // stobop
