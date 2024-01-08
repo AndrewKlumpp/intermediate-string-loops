b71e752a0ea01186dfd8dc1793ba694743820727
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-08 16:12:27 -0700

    finished has three vowels

--- hasThreeVowels.js
+++ hasThreeVowels.js
@@ -1,4 +1,16 @@
-// Your code here
+function hasThreeVowels(str) {
+  let vowels = "aeiou";
+  let diffVowels = "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (vowels.includes(char) && !diffVowels.includes(char)) {
+      diffVowels = diffVowels + char;
+    }
+  } if (diffVowels.length >= 3) {
+    return true;
+  }
+  return false;
+}
 
 
 console.log(hasThreeVowels('delicious'));       //  true
