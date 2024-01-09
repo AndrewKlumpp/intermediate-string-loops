cdfec93756a49e7fd93c605726313a2375cb3e6a
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-08 17:30:33 -0700

    finished double letter count

--- doubleLetterCount.js
+++ doubleLetterCount.js
@@ -1,5 +1,11 @@
 function doubleLetterCount(string) {
-  // your code here
+  let twoInRow = 0;
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (char === string[i-1]) {
+      twoInRow = twoInRow + 1;
+    }
+  } return twoInRow;
 }
 
 console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
