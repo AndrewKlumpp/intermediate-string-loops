829630958dd3ca100a54892b6d9cdd60893dd310
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-08 18:36:04 -0700

    finished vowel cipher

--- vowelCipher.js
+++ vowelCipher.js
@@ -1,5 +1,19 @@
 function vowelCipher(string) {
-  // your code here
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let vowels = "aeiou"
+  let newString = "";
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (!vowels.includes(char)) {
+      newString += char;
+    } else if (vowels.includes(char)) {
+      let origIndex = vowels.indexOf(char);
+      let newIndex = origIndex + 1;
+      let newChar = vowels[newIndex % vowels.length];
+      newString += newChar;
+    }
+  }
+  return newString;
 }
 
 
